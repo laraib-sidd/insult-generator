1a24bafb234517deca66f2ad9345ff1b0e0de5b5
Lance Ellis <scdelance@gmail.com>
2020-09-05 15:11:46 -0500

    Fixed frontend

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,21 @@
 insultBtn = document.querySelector('.insultButton')
 
+let tweetButton = document.getElementById("tweet")
 
 barba.use(barbaCss)
 
 barba.init({
     transitions: [
         {
-            name: "moveUp"
+            name: "moveUp",
+            afterEnter(){
+                // tweetButton.setAttribute("ref", "https://twitter.com/share?ref_src=twsrc%5Etfw")
+                // tweetButton.setAttribute("class", "twitter-share-button")
+                // tweetButton.setAttribute("data-size", "large")
+                // tweetButton.setAttribute("data-text", "Insult goes here")
+                // tweetButton.setAttribute("data-show-count", "false") 
+                //<a ref="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="Insult goes here!" data-show-count="false">Tweet</a>
+            }
         },
     ]
 })
