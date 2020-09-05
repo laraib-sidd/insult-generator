a85736e1cb451dcc3c05e9792e92e8b8ff731eaf
laraib-sidd <laraib.sidd7>
2020-09-05 19:46:24 +0530

    update

--- src/index.js
+++ src/index.js
@@ -8,3 +8,7 @@ const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
+
+app.listen(port, () => {
+    console.log('The server is running on ', port);
+})
