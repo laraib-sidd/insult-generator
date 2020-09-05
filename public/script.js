3ff5186b21b7fb4d72ce40588d92932d2bd31d01
laraib-sidd <laraib.sidd7>
2020-09-05 20:04:57 +0530

    udpate

--- public/script.js
+++ public/script.js
@@ -1,4 +1,1 @@
 const heading = document.querySelector('#mainTitle')
-fetch('https://www.nosweatshakespeare.com/resources/shakespeare-insults/')
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const https = require('https')
 
 const app = express();
 
@@ -8,7 +9,8 @@ const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
-
+https.get('https://www.nosweatshakespeare.com/resources/shakespeare-insults/',
+    (res => console.log(res)))
 app.listen(port, () => {
     console.log('The server is running on ', port);
 })
