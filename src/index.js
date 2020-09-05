a0bb0591f42ed8a49bc9d82d432cf879eefba800
laraib-sidd <laraib.sidd7>
2020-09-05 19:44:52 +0530

    update

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
